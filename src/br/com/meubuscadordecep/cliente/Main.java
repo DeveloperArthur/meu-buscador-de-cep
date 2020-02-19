e551368b79f395007085c2d0a9b2d85653597594
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2020-02-19 15:55:35 -0300

    Add files via upload

--- src/br/com/meubuscadordecep/cliente/Main.java
+++ src/br/com/meubuscadordecep/cliente/Main.java
@@ -3,18 +3,17 @@ package br.com.meubuscadordecep.cliente;
 import br.com.meubuscadordecep.dominio.Endereco;
 import br.com.meubuscadordecep.viacep.ServicoDeCep;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         String cep = new Scanner(System.in).nextLine();
         Endereco endereco = ServicoDeCep.buscaEnderecoPelo(cep);
         System.out.println("Dados do Endereço");
-        System.out.println("Logradouro: "+endereco.getLogradouro());
-        System.out.println("Bairro: "+endereco.getBairro());
-        System.out.println("Localidade: "+endereco.getLocalidade());
+        System.out.println("Logradouro: " + endereco.getLogradouro());
+        System.out.println("Bairro: " + endereco.getBairro());
+        System.out.println("Localidade: " + endereco.getLocalidade());
     }
 
 }
--- src/br/com/meubuscadordecep/viacep/ServicoDeCep.java
+++ src/br/com/meubuscadordecep/viacep/ServicoDeCep.java
@@ -6,13 +6,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.UnknownHostException;
 
 public class ServicoDeCep {
     static String webService = "http://viacep.com.br/ws/";
     static int codigoSucesso = 200;
 
-    public static Endereco buscaEnderecoPelo(String cep) throws UnknownHostException {
+    public static Endereco buscaEnderecoPelo(String cep) throws Exception {
         String urlParaChamada = webService + cep + "/json";
 
         try {
@@ -34,7 +33,7 @@ public class ServicoDeCep {
             conexao.disconnect();
             return new Endereco("", "", "");
         } catch (Exception e) {
-            throw new UnknownHostException("SEM INTERNET " + e);
+            throw new Exception("Ocorreu um erro inesperado: " + e);
         }
     }
 }
